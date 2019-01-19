e3b6126c006c9370434a586c94392c3c28830251
Chris Givens <givenscj@Hotmail.com>
2019-01-18 16:50:49 -0800

    UserProfile and Search changes.

--- SharePoint.cs
+++ SharePoint.cs
@@ -1,15 +1,21 @@
 ﻿using GDPR.Common;
 using GDPR.Common.Classes;
+using Microsoft.Office.Server.Search.Query;
 using Microsoft.Office.Server.UserProfiles;
 using Microsoft.SharePoint;
 using Microsoft.SharePoint.Administration;
+using Newtonsoft.Json;
 using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Data;
 
 namespace GDPR.Applications
 {
     public class SharePoint : BaseGDPROAuthApplication
     {
+        HttpHelper hh = new HttpHelper();
+
         public SharePoint()
         {
             this._version = "1.0.0.0";
@@ -52,6 +58,153 @@ namespace GDPR.Applications
         public override void AnonymizeRecord(Record r)
         {
             base.AnonymizeRecord(r);
+        }
+
+        public dynamic ExecuteApiRequest(string url, string method, string data)
+        {
+            string domain = GetProperty("TenantDomain");
+            string finalUrl = string.Format("https://{0}.sharepoint.com/{1}", domain, url);
+            hh.contentTypeOverride = "application/json";
+            hh.acceptOverride = "application/json";
+            //hh.headers.Add("Authorization", "Bearer " + this.AccessToken);
+            string html = "";
+
+            if (method == "GET")
+                html = hh.DoGet(finalUrl, "");
+
+            if (method == "POST")
+                html = hh.DoPost(finalUrl, data, "");
+
+            if (method == "DELETE")
+                html = hh.DoDelete(finalUrl, data, "");
+
+            dynamic json = JsonConvert.DeserializeObject(html);
+            return json;
+        }
+
+        public List<Record> DoSharePointSearch(GDPRSubject search)
+        {
+            List<Record> records = new List<Record>();
+
+            try
+            {
+                using (SPSite siteCollection = GetFirstSPSite())
+                {
+                    KeywordQuery keywordQuery = new KeywordQuery(siteCollection);
+
+                    foreach (GDPRSubjectEmail se in search.EmailAddresses)
+                    {
+                        keywordQuery.QueryText = se.EmailAddress;
+                        SearchExecutor searchExecutor = new SearchExecutor();
+                        ResultTableCollection resultTableCollection = searchExecutor.ExecuteQuery(keywordQuery);
+                        ResultTable resultTable = resultTableCollection.Filter("TableType", KnownTableTypes.RelevantResults).FirstOrDefault();
+                        DataTable dataTable = resultTable.Table;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //GDPRCore.Current.Log(ex, LogLevel.Error);
+            }
+
+            return records;
+        }
+
+        public List<Record> DoProfileSearch(GDPRSubject search)
+        {
+            List<Record> records = new List<Record>();
+
+            //do userprofile
+            foreach (GDPRSubjectEmail se in search.EmailAddresses)
+            {
+                Record r = new Record();
+
+                string loginName = se.EmailAddress;
+
+                //testing 
+                loginName = "administrator@sanspug.org";
+                //loginName = Utility.UrlEncode(loginName);
+
+                UserProfileManager mgr = GetUserProfileManager();
+                UserProfile up = mgr.GetUserProfile(Guid.Parse("82712b6f-bfe0-4353-905e-0539d7dcc027"));
+                //UserProfile up = mgr.GetUserProfile(loginName);
+                string data = Utility.SerializeObject(up.Properties, 1);
+                //r.AdminLinkUrl = up.PersonalUrl.ToString();
+                //r.LinkUrl = up.PersonalUrl.ToString();
+                r.ApplicationId = Guid.Parse(GDPR.Common.Configuration.ApplicationId);
+                r.Object = data;
+                r.Type = "UserProfile";
+                r.RecordId = up.ID.ToString();
+                r.RecordDate = up.PersonalSiteLastCreationTime;
+
+                ValidateRecord(r);
+
+                records.Add(r);
+            }
+
+            return records;
+        }
+
+        public override List<Record> GetAllRecords(GDPRSubject search)
+        {
+            List<Record> records = new List<Record>();
+
+            SPSecurity.RunWithElevatedPrivileges(delegate ()
+            {
+                //do search
+                records.AddRange(DoSharePointSearch(search));
+
+                //do profile
+                records.AddRange(DoProfileSearch(search));
+            });
+
+            return records;
+        }
+
+        public SPSite GetFirstSPSite()
+        {
+            SPFarm farm = SPFarm.Local;
+
+            SPWebService service = farm.Services.GetValue<SPWebService>("");
+
+            foreach (SPWebApplication webApp in service.WebApplications)
+            {
+                foreach (SPSite site in webApp.Sites)
+                {
+                    return site;
+                }
+            }
+
+            return null;
+        }
+
+        public UserProfileManager GetUserProfileManager()
+        {
+            SPFarm farm = SPFarm.Local;
+
+            SPWebService service = farm.Services.GetValue<SPWebService>("");
+
+            foreach (SPWebApplication webApp in service.WebApplications)
+            {
+                foreach (SPSite site in webApp.Sites)
+                {
+                    //get user profiles...
+                    SPServiceContext serviceContext = SPServiceContext.GetContext(site);
+
+                    try
+                    {
+                        UserProfileManager userProfileMgr = new UserProfileManager(serviceContext);
+                        return userProfileMgr;
+                    }
+                    catch (System.Exception e)
+                    {
+                        Console.WriteLine(e.GetType().ToString() + ": " + e.Message);
+                        Console.Read();
+                    }
+                }
+            }
+
+            return null;
         }
 
         public override List<GDPRSubject> GetAllSubjects(int skip, int count, DateTime? changeDate)
@@ -144,6 +297,5 @@ namespace GDPR.Applications
 
             return subjects;
         }
-
     }
 }
